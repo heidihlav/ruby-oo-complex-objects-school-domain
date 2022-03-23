bbe2cf35cdbfdb5461019c8e3e237b4b06e60135
Heidi Hlavinka <hchlav@gmail.com>
2022-03-23 14:01:36 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
 # code here!
+require 'pry'
+
+class School
+  
+  def initialize(name)
+    @roster = {}
+  end
+
+  def roster=(roster)
+    @roster = roster
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    # unless @roster[grade]
+    #   @roster[grade] = [] 
+    # end
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+    def grade(number)
+      roster[number]
+    end
+
+    def sort
+      hash = {}
+      roster.keys.sort.each do |number|
+        hash[number] = roster[number].sort
+      end
+        hash
+    end
+    
+end
+
+# binding.pry
